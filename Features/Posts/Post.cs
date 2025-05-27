f674c713c9949c0cb702cbf2a00bc87026709853
hageranter <138611708+hageranter@users.noreply.github.com>
2025-05-27 19:15:34 +0300

    Update changes and add new files

--- Features/Posts/Post.cs
+++ Features/Posts/Post.cs
@@ -2,14 +2,14 @@ namespace FileBlogApi.Features.Posts;
 
 public class Post
 {
-   public string Slug { get; set; } = "";
-    public string FolderName { get; set; } = ""; // The folder name where the post is stored
-    public string Title { get; set; } = "";
-    public string Description { get; set; } = "";
+   public string Slug =string.Empty;
+    public string FolderName =string.Empty; // The folder name where the post is stored
+    public string Title =string.Empty;
+    public string Description =string.Empty;
     public DateTime PublishedDate { get; set; }
     public DateTime ModifiedDate { get; set; }
-    public List<string> Tags { get; set; } = new List<string>();
-    public List<string> Categories { get; set; } = new List<string>();
-    public string Body { get; set; } = "";
-    public List<string> AssetFiles { get; set; } = new List<string>();
+    public List<string> Tags { get; set; } = [];
+    public List<string> Categories { get; set; } = [];
+    public string Body =string.Empty;
+    public List<string> AssetFiles { get; set; } = [];
 }
